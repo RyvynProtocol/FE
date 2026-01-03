c6a0f6e9a1b4345f80c7da87b20aeb6c167cb49f
Arif <arifamjy@gmail.com>
2026-01-03 09:40:21 +0700

    chore: initialize shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
+import {  Figtree } from "next/font/google";
 import './globals.css';
 import { PrivyProvider } from '@/providers/PrivyProvider';
+
+const figtree = Figtree({
+  subsets:['latin'],
+  variable:'--font-sans'
+});
 
 export const metadata: Metadata = {
   title: 'USDC-B | Stablecoin with Built-in Yield Rewards',
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className={`antialiased ${figtree.className}`}>
         <PrivyProvider>{children}</PrivyProvider>
       </body>
     </html>
